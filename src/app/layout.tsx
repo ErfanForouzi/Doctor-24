b94f050e6fa75562ccde344a6b56070e3308d417
Erfan Forouzi <erfanforouzzziii@gmail.com>
2026-01-03 16:22:20 +0330

    REMOVE unnecessary codes and files - ADD vazirmatn font - ADD hello,friends

---
+++ src/app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import {Vazirmatn} from "next/font/google"
+import "./globals.css";
+
+
+const vazirmatn = Vazirmatn({
+  subsets: ['latin','arabic'],
+  display: 'swap',
+})
+
+export const metadata: Metadata = {
+  title: "دکتر 24",
+  description: "دستیابی به دکتر های خوب در دکتر 24",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html className={`${vazirmatn.className}`} lang="fa" dir="rtl">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div>
+      <h1>سلام رفیق</h1>
+    </div>
+  );
+}
