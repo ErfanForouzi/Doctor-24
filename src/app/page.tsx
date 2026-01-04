2e7ff12dd8af788e8f39dba6c34ce506cc7db2a3
Erfan Forouzi <erfanforouzzziii@gmail.com>
2026-01-04 11:04:29 +0330

    ADD dynamic fetch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,24 @@
+type Post = {
+  id:number,
+  title:string
+}
 
-export default function Home() {
+
+export default async function Home() {
+  const response = await fetch (`https://jsonplaceholder.typicode.com/posts`,{
+    cache:"no-store"
+  });
+  const posts:Post[] = await response.json();
   return (
     <div>
       <h1>سلام رفیق</h1>
+      <ul>
+        {posts.map((post)=>(
+          <li key={post.id}>
+            {post.title}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
