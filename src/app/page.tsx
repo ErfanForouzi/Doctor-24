aba3d695d24d18d32562b24539c0dad4d9900de3
Erfan Forouzi <erfanforouzzziii@gmail.com>
2026-01-05 01:41:06 +0330

    ADD home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,25 @@
+import GlobalSearchBoxComponent from "@/components/global-search-box/global-search-box.component";
 
+import styles from "./page.module.css"
+import MyDoctorLogo from "@/logo/my-doctor.logo";
 export default async function Home() {
 
   return (
-    <div>
-      <h1>سلام رفیق</h1>
+    <div className={styles.home}>
+      <h1>
+        <MyDoctorLogo />
+        دکتر من
+      </h1>
+      <GlobalSearchBoxComponent />
+      <div className={styles.history}>
+        <div className={styles.title}>آخرین جستجوهای شما</div>
+        <ul>
+          <li>ارتوپد</li>
+          <li>قلب و عروق</li>
+        </ul>
+      </div>
+
     </div>
   );
+
 }
