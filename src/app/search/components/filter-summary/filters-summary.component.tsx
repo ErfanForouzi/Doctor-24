8d80e0f9b0d0839a3a49f33f3688d240bc406b2d
Erfan Forouzi <erfanforouzzziii@gmail.com>
2026-01-24 12:57:43 +0330

    [UPDATE] filter-summary component

--- src/app/search/components/filter-summary/filters-summary.component.tsx
+++ src/app/search/components/filter-summary/filters-summary.component.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ReactElement } from "react";
+import { ReactElement, useContext, useMemo } from "react";
 
 
 import { ButtonComponent } from "@/components/button/button.component";
@@ -8,9 +8,29 @@ import CardComponent from "@/components/card/card.component";
 
 
 import styles from "./filters-summary.module.css";
+import { FilterContext } from "../../providers/filters/filters.provider";
+import { FiltersType } from "@/types/filters.types";
 
 export default function FiltersSummaryComponent(): ReactElement | null {
 
+  const { filters, dispatchFilters } = useContext(FilterContext)
+
+  const isEmpty = useMemo(() => {
+    return (!filters.degree && !filters.query && !filters.gender && !filters.expertise)
+  }, [filters])
+
+  const removeAllButtonClickHandler = (): void => {
+    dispatchFilters({ type: "removed_all" });
+  };
+
+  const filterClickHandler = (key: keyof FiltersType): void => {
+    dispatchFilters({ type: "removed_filter", key });
+  };
+
+
+  if (isEmpty) {
+    return null
+  }
 
   return (
     <CardComponent>
@@ -21,15 +41,17 @@ export default function FiltersSummaryComponent(): ReactElement | null {
           variant="danger"
           shape="outlined"
           type="button"
+          onClick={removeAllButtonClickHandler}
         >
           حذف همه
         </ButtonComponent>
 
         <ul className={styles.filters}>
-            <li> query</li>
-            <li> expertise</li>
-            <li> gender</li>
-            <li> degree</li>
+          {filters.query && <li onClick={() => filterClickHandler("query")}> {filters.query}</li>}
+          {filters.expertise && <li onClick={() => filterClickHandler("expertise")}> {filters.expertise}</li>}
+          {filters.gender && <li onClick={() => filterClickHandler("gender")}> {filters.gender}</li>}
+          {filters.degree && <li onClick={() => filterClickHandler("degree")}> {filters.degree}</li>}
+
         </ul>
       </div>
     </CardComponent>
