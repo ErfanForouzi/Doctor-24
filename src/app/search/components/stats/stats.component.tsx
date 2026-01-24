0d57d8d149eb0eac913c8a2014073513d74c311b
Erfan Forouzi <erfanforouzzziii@gmail.com>
2026-01-24 13:00:48 +0330

    [UPDATE] stats component

--- src/app/search/components/stats/stats.component.tsx
+++ src/app/search/components/stats/stats.component.tsx
@@ -7,11 +7,10 @@ import { DoctorsContext } from "@/app/search/providers/doctors/doctors.provider"
 import styles from "./stats.module.css";
 
 export default function StatsComponent(): ReactElement {
-
-  return (
-    <div className={styles.stats}>
-      {/* {filteredDoctors.length.toLocaleString()} نتیجه */}
-       نتیجه
-    </div>
-  );
+    const { filteredDoctors } = useContext(DoctorsContext)
+    return (
+        <div className={styles.stats}>
+            {filteredDoctors.length} نتیجه
+        </div>
+    );
 }
