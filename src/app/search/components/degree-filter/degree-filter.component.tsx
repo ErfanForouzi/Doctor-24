a82ddd3fec7c1149c3424903618a944eba499cca
Erfan Forouzi <erfanforouzzziii@gmail.com>
2026-01-24 12:47:45 +0330

    [UPDATE] degree-filter component

--- src/app/search/components/degree-filter/degree-filter.component.tsx
+++ src/app/search/components/degree-filter/degree-filter.component.tsx
@@ -1,27 +1,31 @@
 "use client";
 
-import { ReactElement } from "react";
+import { ReactElement, useContext } from "react";
 
 import RadioFilterComponent from "@/app/search/components/radio-filter/radio-filter.component";
+import { FilterContext } from "../../providers/filters/filters.provider";
 
 export default function DegreeFilterComponent(): ReactElement {
+    const { filters, dispatchFilters } = useContext(FilterContext)
+    const changeHandler = (value: string): void => {
+        dispatchFilters({ type: "updated_filter", key: "degree", value })
+    }
 
-
-  return (
-    <RadioFilterComponent
-      title="درجه علمی"
-      name="degree"
-      options={[
-        { value: "فلوشیپ", label: "فلوشیپ" },
-        { value: "فوق تخصص", label: "فوق تخصص" },
-        { value: "دکترای تخصصی", label: "دکترای تخصصی" },
-        { value: "متخصص", label: "متخصص" },
-        { value: "دکتری", label: "دکتری" },
-        { value: "کارشناس ارشد", label: "کارشناس ارشد" },
-        { value: "کارشناس", label: "کارشناس" },
-      ]}
-    //   value={filters.degree}
-    //   onChange={changeHandler}
-    />
-  );
+    return (
+        <RadioFilterComponent
+            title="درجه علمی"
+            name="degree"
+            options={[
+                { value: "فلوشیپ", label: "فلوشیپ" },
+                { value: "فوق تخصص", label: "فوق تخصص" },
+                { value: "دکترای تخصصی", label: "دکترای تخصصی" },
+                { value: "متخصص", label: "متخصص" },
+                { value: "دکتری", label: "دکتری" },
+                { value: "کارشناس ارشد", label: "کارشناس ارشد" },
+                { value: "کارشناس", label: "کارشناس" },
+            ]}
+            value={filters.degree}
+            onChange={changeHandler}
+        />
+    );
 }
